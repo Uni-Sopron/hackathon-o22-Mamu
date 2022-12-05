a1cb9e4781b2daa73f1e660e8d33045e5c9c17cb
hbela <hbela2000@gmail.com>
2022-12-05 14:41:02 +0100

    Megjelenődnek az eddig felvitt player

--- frontend/src/Pages/First.js
+++ frontend/src/Pages/First.js
@@ -1,48 +1,56 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { useDataContext } from '../DataProvider'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDataContext } from "../DataProvider";
 
 export const First = () => {
-    const {
-        kartyak,
-        setKartyak,
-        tipsDreamer,
-        setTipsDreamer,
-        tipsPlayers,
-        setTipsPlayers,
-        players,
-        setPlayers
-    } = useDataContext()
+  const {
+    kartyak,
+    setKartyak,
+    tipsDreamer,
+    setTipsDreamer,
+    tipsPlayers,
+    setTipsPlayers,
+    players,
+    setPlayers,
+  } = useDataContext();
 
-    const [data, setData] = useState()
+  const [data, setData] = useState();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function addPlayers() {
-        /* Maximum 10 játékos */
-        if (players.length <= 9) {
-            setPlayers([...players, data])
-        }
+  function addPlayers() {
+    /* Maximum 10 játékos */
+    if (players.length <= 9) {
+      setPlayers([...players, data]);
     }
+  }
 
-    function minPlayers() {
-        /* Legalább 4 játékos */
-        if(players.length >= 4) {
-            navigate('/Second')
-        }
+  function minPlayers() {
+    /* Legalább 4 játékos */
+    if (players.length >= 4) {
+      navigate("/Second");
     }
-    
-    console.log(players)
-    return (
-        <div>
-            <input type="text" onChange={(event) => { 
-                setData({
-                    name: event.target.value,
-                    whois: ""
-                });
-             }} />
-             <button onClick={addPlayers}>Add</button>
-            <button onClick={minPlayers}>Start</button>
-        </div>
-    );
-}
+  }
+
+  console.log(players);
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(event) => {
+          setData({
+            name: event.target.value,
+            whois: "",
+          });
+        }}
+      />
+      <button onClick={addPlayers}>Add</button>
+      <button onClick={minPlayers}>Start</button>
+      <div>
+        {players.map((player, index) => (
+          <p>{player.name}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
